9ebebb8d08f55dd1bec49e582b80defa5ff644ce
chita-co <chita.co2528@gmail.com>
2026-08-11 20:56:24 +0330

    feat: add 301 redirect from vercel.app to sabzfaraz.ir

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,21 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "**.arvanstorage.ir" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "sabzfaraz.vercel.app",
+          },
+        ],
+        destination: "https://sabzfaraz.ir/:path*",
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
