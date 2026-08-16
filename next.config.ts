671370259ffada1658924bd030039daecf38e616
chita-co <chita.co2528@gmail.com>
2026-08-16 14:59:10 +0330

    add enamad verification page and proxy exemption

--- next.config.ts
+++ next.config.ts
@@ -10,21 +10,6 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "**.arvanstorage.ir" },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "sabzfaraz.vercel.app",
-          },
-        ],
-        destination: "https://sabzfaraz.ir/:path*",
-        permanent: true, // 301 redirect
-      },
-    ];
-  },
 };
 
 export default nextConfig;
