dcb0c2fd0786cb63766e03955e4564883f15568d
chita-co <chita.co2528@gmail.com>
2026-08-27 22:22:40 +0330

    fix: use custom support icon and remove unused import

--- src/components/shop/FloatingSupportButton.tsx
+++ src/components/shop/FloatingSupportButton.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import Link from "next/link";
-import { LifeBuoy } from "lucide-react";
 import SupportUnreadBadgeUser from "./SupportUnreadBadgeUser";
 
 export default function FloatingSupportButton() {
   return (
     <Link href="/support" className="floating-support-btn" aria-label="پشتیبانی" >
-      <LifeBuoy size={22} />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/p-icon.png"
+        alt="پشتیبانی"
+        width={28}
+        height={28}
+        style={{ objectFit: "contain", borderRadius: "50%" }}
+      />
       <SupportUnreadBadgeUser />
     </Link>
   );
