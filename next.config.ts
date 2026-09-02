432fce1ea7be41aa408fb054dc8643c79b0e191b
chita-co <chita.co2528@gmail.com>
2026-09-02 10:46:36 +0330

    Fix payment callback session refresh

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-  "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self'; connect-src 'self' https: https://www.google-analytics.com https://www.googletagmanager.com;",
+  "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: blob: https:; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https: https://www.google-analytics.com https://www.googletagmanager.com; frame-src 'self' https://sabzfaraz.vercel.app;",
           },
         ],
       },
