b704bab98b96bec7f466705c3e7e3a3c2a3d9d2d
chita-co <chita.co2528@gmail.com>
2026-09-04 17:22:48 +0330

    Set store name text to static value on shipping label

--- src/components/admin/AdminShippingLabelView.tsx
+++ src/components/admin/AdminShippingLabelView.tsx
@@ -167,7 +167,7 @@ export default function AdminShippingLabelView({
             <div className="sl-center">
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img src="/logo-invoice.png" alt={storeName} style={{ height: 28, marginBottom: 2 }} />
-              <h2 className="sl-brand">{storeName}</h2>
+              <h2 className="sl-brand">فروشگاه سبزفراز</h2>
               <p className="sl-brand-site"><span dangerouslySetInnerHTML={{ __html: ICONS.globe }} /> sabzfaraz.ir</p>
             </div>
 
