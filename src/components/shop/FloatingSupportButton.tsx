a925fb7d0b5c2911a0781138e8a4a736bae56f2f
chita-co <chita.co2528@gmail.com>
2026-09-08 13:58:18 +0330

    Update support section: improve ticket

--- src/components/shop/FloatingSupportButton.tsx
+++ src/components/shop/FloatingSupportButton.tsx
@@ -5,20 +5,22 @@ import SupportUnreadBadgeUser from "./SupportUnreadBadgeUser";
 
 export default function FloatingSupportButton() {
   return (
-    <Link href="/support" className="floating-support-btn" aria-label="پشتیبانی" >
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/p-icon.png"
-        alt="پشتیبانی"
-        style={{
-          width: "165%",
-          height: "165%",
-          objectFit: "cover",
-          objectPosition: "center 25%",
-          display: "block",
-        }}
-      />
+    <div className="floating-support-wrap">
+      <Link href="/support" className="floating-support-btn" aria-label="پشتیبانی" >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/p-icon.png"
+          alt="پشتیبانی"
+          style={{
+            width: "165%",
+            height: "165%",
+            objectFit: "cover",
+            objectPosition: "center 25%",
+            display: "block",
+          }}
+        />
+      </Link>
       <SupportUnreadBadgeUser />
-    </Link>
+    </div>
   );
 }
